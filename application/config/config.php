8c16df126a593a6b75e5bdfea4bf0a08303e27df
devshafiul <dshafiul@netmediamobile.com>
2019-05-23 14:14:56 +0600

    Base url, library and config updated in update config folder.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci_hmvc/';
+$config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -34,7 +34,7 @@ $config['base_url'] = 'http://localhost/ci_hmvc/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
